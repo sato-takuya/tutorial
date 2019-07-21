79aa190b9d319e936fee7bf4ce1a93e56bbfd92c
SATOTAKUYA <stakuya6@gmail.com>
2019-07-21 16:40:45 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,34 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
-  #validates(:name, {presence: true}) と同じ
-
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
-  before_save{self.email = email.downcase}
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
